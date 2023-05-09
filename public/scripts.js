bb9010be6b167dd9de3dbe20b56dfae8cc037de4
Meng Wu <kaiwu.email.unc.edu>
2023-05-09 10:55:01 -0400

    Show results in html dialog

--- public/scripts.js
+++ public/scripts.js
@@ -24,11 +24,20 @@ async function playGame() {
     let baseurl = document.location.href + 'app/'
     let url = baseurl + game + '/play'
     if(gameType == "opponent") {
-        url += '/' + document.querySelector('input[name="shot"]:checked').value
+        url += '/' + shot
     }
     let response = await fetch(url)
     let data = await response.json()
-    
+    if (gameType == "opponent") {
+        let player = "You: " + data.player + "<br>"
+        let opponent = "Opponent: " + data.opponent + "<br>"
+        let result = "Result: " + data.result
+        document.getElementById("result").innerHTML = player + opponent + result
+    } else {
+        let player = data.player.toUpperCase()
+        document.getElementById("result").innerHTML = player
+    }
+    document.getElementById("results").showModal()
 }
 
 async function toggleOpponent() {
@@ -53,6 +62,7 @@ async function toggleGame() {
 
 async function selectChoice(elem) {
     chosenShot = "chosen" + elem.id
+    shot = elem.id
     resetShotChoice()
     document.getElementById(chosenShot).hidden = false
 }
@@ -62,4 +72,8 @@ async function resetShotChoice(){
     for (let button of buttons) {
         button.hidden = true
     }
+}
+
+async function closeDialog() {
+    document.getElementById("results").close()
 }

3494fa12da696dc6f22922cb963ae6f6883ef730
Meng Wu <kaiwu.email.unc.edu>
2023-05-08 23:27:11 -0400

    Add buttons to choose shot

--- public/scripts.js
+++ public/scripts.js
@@ -5,17 +5,41 @@ var game, gameType
 document.addEventListener("DOMContentLoaded", function() {  
     game = document.querySelector('input[name="game"]:checked').value
     gameType = document.querySelector('input[name="gameType"]:checked').value
+    toggleGame()
+    toggleOpponent()
 })
 
 async function selectGame(elem) {
     game = elem.value
+    toggleGame()
 }
 
 async function selectGameType(elem) {
     gameType = elem.value
+    toggleOpponent()
 }
 
 async function playGame() {
     console.log(game)
     console.log(gameType)
+}
+
+async function toggleOpponent() {
+    if(gameType == "opponent") {
+        document.getElementsByClassName("choices")[0].hidden = false
+        document.getElementsByClassName("chosenChoices")[0].hidden = false
+    } else {
+        document.getElementsByClassName("choices")[0].hidden = true
+        document.getElementsByClassName("chosenChoices")[0].hidden = true
+    }
+}
+
+async function toggleGame() {
+    if(game == "rps") {
+        document.getElementById("lizard").hidden = true
+        document.getElementById("spock").hidden = true
+    } else {
+        document.getElementById("lizard").hidden = false
+        document.getElementById("spock").hidden = false
+    }
 }
